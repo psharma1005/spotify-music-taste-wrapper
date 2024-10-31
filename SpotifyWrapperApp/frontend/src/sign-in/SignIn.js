29a7b61792b5fbc5729525364888b1116353eb3a
uedaritsu534 <uedaritsu534@gmail.com>
2024-10-31 14:58:25 -0400

    deleted picture on signin

--- SpotifyWrapperApp/frontend/src/sign-in/SignIn.js
+++ SpotifyWrapperApp/frontend/src/sign-in/SignIn.js
@@ -120,7 +120,7 @@ export default function SignIn(props) {
       <SignInContainer direction="column" justifyContent="space-between">
         <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
         <Card variant="outlined">
-          <SitemarkIcon />
+
           <Typography
             component="h1"
             variant="h4"
