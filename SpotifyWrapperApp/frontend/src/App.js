112cebc664bb267b1ad16d8a0e570ca53b1ac5ef
RSeeth947 <rupesh.seeth@gmail.com>
2024-10-30 22:18:24 -0400

    addeed test component and views for future reference

--- SpotifyWrapperApp/frontend/src/App.js
+++ SpotifyWrapperApp/frontend/src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Button from "@mui/material/Button"
 import SignIn from "./sign-in/SignIn"
+import Test from './testUI/Test';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <SignIn />
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<SignIn />} />
+          <Route path='/login' element={<SignIn />} />
+          <Route path='/test' element={<Test />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
