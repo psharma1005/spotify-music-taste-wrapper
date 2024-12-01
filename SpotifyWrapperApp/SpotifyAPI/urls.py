66ad7105b97ef0038674aa520838cc003c901c49
psharma1005 <psharma388@gatech.edu>
2024-11-30 22:43:23 -0500

    Updated with more methods.

--- SpotifyWrapperApp/SpotifyAPI/urls.py
+++ SpotifyWrapperApp/SpotifyAPI/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     path('login/', views.spotify_login, name='spotify-login'),
     path('callback/', views.spotify_callback, name='spotify-callback'),
-    path('top-artists/', views.user_top_artists, name='user-top-artists'),
-    path('top-tracks/', views.user_top_tracks, name='user-top-tracks'),
+    path('top-artists/', views.get_top_artists, name='get_top_artists'),
+    path('top-tracks/', views.get_top_tracks, name='get_top_tracks'),
+    path('top-genres/', views.get_top_genres, name='get_top_genres'),
+    path('new-artists/', views.get_new_artists, name='get_new_artists'),
+    path('create-playlist/', views.create_most_listened_playlist, name='create_playlist'),
+    path('listening-stats/', views.get_listening_stats, name='listening_stats'),
 ]
