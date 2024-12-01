93705ada38ff8d0fe2f5c2e2830d81ab8965e00b
RSeeth947 <rupesh.seeth@gmail.com>
2024-12-01 15:46:29 -0500

    fixes

--- SpotifyWrapperApp/authApp/urls.py
+++ SpotifyWrapperApp/authApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),  # Set /auth/ to display the login form
+    #path('', views.login_view, name='login'),  # Set /auth/ to display the login form
     path('login/', views.login_view, name='login'),  # Optional: Keep this if /auth/login/ is needed
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),  # Add this route for user registration
