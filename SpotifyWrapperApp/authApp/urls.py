b7ecfb3adbd6a161a52411faca74dfb22e97dfeb
msatish6 <msatish6@gatech.edu>
2024-12-01 14:31:17 -0500

    Made login, added delete acc, and testing html

--- SpotifyWrapperApp/authApp/urls.py
+++ SpotifyWrapperApp/authApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.login_view, name='login'),  # Set /auth/ to display the login form
     path('login/', views.login_view, name='login'),  # Optional: Keep this if /auth/login/ is needed
-    path('register/', views.register_view, name='register'),  # Add this route for user registration
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),  # Add this route for user registration
 ]
